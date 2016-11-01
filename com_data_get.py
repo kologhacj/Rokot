fec50185d0cbdef22e0cbe3c80b32ad8fb728668
kologhacj <kostya.klol71@gmail.com>
2016-11-01 14:13:54 +0300

    Initial commit

---
+++ com_data_get.py
@@ -0,0 +1,9 @@
+import serial #Import Serial Library
+ 
+arduinoSerialData = serial.Serial('com3',9600)
+ 
+ 
+while True:
+    if (arduinoSerialData.inWaiting()>0):
+        myData = arduinoSerialData.readline()
+        print(myData.decode("utf-8"))
