ae5a33e0ad5d775df0cf237478f0c22195c1994a
kologhacj <kostya.klol71@gmail.com>
2016-11-01 15:22:39 +0300

    +test

--- com_data_get.py
+++ com_data_get.py
@@ -5,7 +5,10 @@ class COMPort():
     def __init__(self, port, bitrate):
         self.port = port
         self.bitrate = bitrate
-        self.struct = serial.Serial(port, bitrate)
+        try:
+            self.struct = serial.Serial(port, bitrate)
+        except:
+            raise("Connection error")
 
     def serialGet(self):
         return self.struct
@@ -15,24 +18,22 @@ class COMPort():
         fout.write(struct)
         fout.close()
 
-    def readBytes(self, count, write = False, filename = False):
-        try:
-            connect = COMPort("com3", 9600)
-        except:
-            return "Connection Error"
-
-        serial = connect.serialGet() # получить данные из COMPORT в данный момент
+    def readBytes(self, count, write = False, filename = ""):
+        serial = self.serialGet() # получить данные из COMPORT в данный момент
 
         i = 0
         writeFile = ""
 
-        while i <= count:
+        while i < count:
             if (serial.inWaiting() > 0):
                 myData = serial.readline().decode("utf-8")
                 print(myData)
                 writeFile += myData
                 i += 1
         if write:
-            connect.serialWrite(filename, writeFile)
+            self.serialWrite(filename, writeFile)
 
-if  __name__ ==  "__main__" :
+
+if __name__ == "__main__":
+    test = COMPort("com3", 9600)
+    test.readBytes(5, True, "log1.txt")
