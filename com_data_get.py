9560550b45e74f59f4f8e3fe0c1f3e2822224f05
kologhacj <kostya.klol71@gmail.com>
2016-11-01 15:15:05 +0300

    Creating Class

--- com_data_get.py
+++ com_data_get.py
@@ -1,10 +1,38 @@
-import serial #Import Serial Library
- 
-arduinoSerialData = serial.Serial('com3',9600)
- 
- 
-while True:
-    if (arduinoSerialData.inWaiting()>0):
-        myData = arduinoSerialData.readline()
-        print(myData.decode("utf-8"))
-        #Привет2
+import serial  # Import Serial Library
+import msvcrt
+
+class COMPort():
+    def __init__(self, port, bitrate):
+        self.port = port
+        self.bitrate = bitrate
+        self.struct = serial.Serial(port, bitrate)
+
+    def serialGet(self):
+        return self.struct
+
+    def serialWrite(self, file, struct  ):
+        fout = open(file, "w")
+        fout.write(struct)
+        fout.close()
+
+    def readBytes(self, count, write = False, filename = False):
+        try:
+            connect = COMPort("com3", 9600)
+        except:
+            return "Connection Error"
+
+        serial = connect.serialGet() # получить данные из COMPORT в данный момент
+
+        i = 0
+        writeFile = ""
+
+        while i <= count:
+            if (serial.inWaiting() > 0):
+                myData = serial.readline().decode("utf-8")
+                print(myData)
+                writeFile += myData
+                i += 1
+        if write:
+            connect.serialWrite(filename, writeFile)
+
+if  __name__ ==  "__main__" :
